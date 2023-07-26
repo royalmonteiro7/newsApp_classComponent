984085cc0e7d9caac7347850e9bcd27f3f6f8db9
royalmonteiro7 <royalmonteiro7@gmail.com>
2023-07-26 10:56:02 +0530

    fist commit

---
+++ src/components/Spinner.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import loading from './Spinner-2.gif'
+
+export default class Spinner extends Component {
+  render() {
+    return (
+      <div className='text-center'>
+        <img className='my-3' src={loading} alt="loading" />
+      </div>
+    )
+  }
+}
